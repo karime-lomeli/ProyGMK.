ea4a38dd970f48353fa922ea8205847a8e323810
karime-lomeli <75227467+karime-lomeli@users.noreply.github.com>
2021-05-04 14:44:43 -0500

    Pasos

---
+++ src/app/app.module.ts
@@ -0,0 +1,33 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { AboutComponent } from './about/about.component';
+import { BuscadorComponent } from './buscador/buscador.component';
+import { HeroeComponent } from './heroe/heroe.component';
+import { HeroesComponent } from './heroes/heroes.component';
+import { HomeComponent } from './home/home.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { HeroeService } from './shared/heroe.service';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    AboutComponent,
+    BuscadorComponent,
+    HeroeComponent,
+    HeroesComponent,
+    HomeComponent,
+    NavbarComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [
+    HeroeService
+  ],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
