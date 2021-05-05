a55b4b032aa1ce66af18a871b0f01adcc785f2a7
GiovannaZapata <75220780+GiovannaZapata@users.noreply.github.com>
2021-05-04 23:43:43 -0500

    avance del punto 20 al 29

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { from } from 'rxjs';
+
+import { HeroeService, Heroe } from '../shared/heroe.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-heroes',
@@ -7,9 +11,13 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HeroesComponent implements OnInit {
 
-  constructor() { }
+  heroes: Heroe[]; //array del tipo de Heroe
+
+  //habilitar el uso del servicio y el router en los parametros del constructor
+  constructor(public servicio: HeroeService, public router: Router) { }
 
   ngOnInit(): void {
+    this.heroes = this.servicio.getHeroes();
   }
 
 }
