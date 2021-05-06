e9f47204dfded65a0c59f36fa3a3ecdda89fbab6
moni12raid <moni12raid@gmail.com>
2021-05-06 10:33:38 -0500

    hasta el 39

--- src/app/buscador/buscador.component.ts
+++ src/app/buscador/buscador.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { HeroeService, Heroe } from './../shared/heroe.service';
 
 @Component({
   selector: 'app-buscador',
@@ -7,7 +9,20 @@ import { Component, OnInit } from '@angular/core';
 })
 export class BuscadorComponent implements OnInit {
 
-  constructor() { }
+  nombre: string;
+  index: number;
+  heroe: Heroe;
+
+  constructor(private HeroeService: HeroeService, private ActivatedRoute: ActivatedRoute) { 
+    this.ActivatedRoute.params.subscribe(params => {
+      this.nombre = params['nombre'];
+      this.index = HeroeService.buscarHeroe(this.nombre);
+      console.log(this.index);
+      if(this.index !== -1){
+        this.heroe = this.HeroeService.getHeroe(this.index);
+      }
+    });
+  }
 
   ngOnInit(): void {
   }
